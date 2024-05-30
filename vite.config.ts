237cf5ae0d6f03f3d5c76407bab4f6d45e4782e7
Vegatality <vegatality@gmail.com>
2024-05-30 12:48:19 +0900

    move: move lib files to lib folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,43 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import path from 'path';
 import dts from 'vite-plugin-dts';
+import { extname, relative, resolve } from 'path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), dts()],
+  plugins: [react(), tsconfigPaths(), dts({ include: 'lib' })],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'lib/index.ts'),
       name: '@vegatality/react-hook-modal',
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      input: Object.fromEntries(
+        glob
+          .sync('lib/**/*.{ts,tsx}', {
+            ignore: ['lib/**/*.d.ts'],
+          })
+          .map((file) => [
+            // The name of the entry point
+            // lib/nested/foo.ts becomes nested/foo
+            relative('lib', file.slice(0, file.length - extname(file).length)),
+            // The absolute path to the entry file
+            // lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ]),
+      ),
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
         },
+        // assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
       },
       // @see https://github.com/vitejs/vite/issues/15012#issuecomment-1948550039
       onwarn(warning, defaultHandler) {
